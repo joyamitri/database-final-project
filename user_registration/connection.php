dc47b52a55b2c1a2f2934a1f91bfeefd88cad6f7
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2022-05-03 22:37:01 +0300

    Update connection.php

--- user_registration/connection.php
+++ user_registration/connection.php
@@ -1,57 +1,97 @@
 <?php
 session_start();
 header('location:page-login.php');
-$conn = mysqli_connect('localhost', 'root', '');
 
-mysqli_select_db($conn, 'companydb');
+$conn = new mysqli('localhost', 'root', '', 'companydb');
 
-$first_name=$_POST['first_name'];
-$last_name=$_POST['last_name'];
-$username=$_POST['username'];
+if(!$conn){
+
+	echo "There are some problems connecting to the database";
+}
+
+$first_name=mysqli_real_escape_string($conn,$_POST['first_name']);
+$last_name=mysqli_real_escape_string($conn,$_POST['last_name']);
+$username=mysqli_real_escape_string($conn,$_POST['username']);
 $email=$_POST['email'];
-$password=$_POST['password'];
-$company=$_POST['company_name'];
-$location=$_POST['location'];
+$password=password_hash(mysqli_real_escape_string($conn,$_POST['password']), PASSWORD_DEFAULT);
+$company_name=mysqli_real_escape_string($conn,$_POST['company_name']);
+$location=mysqli_real_escape_string($conn,$_POST['location']);
 
 $checker = "SELECT * FROM users WHERE username='$username'";
 $result=mysqli_query($conn, $checker);
 
 $num=mysqli_num_rows($result);
-if($num == 1){
 
-	echo "Username Already Taken.";
+if($num == 1) {
+
+	echo "Username Already Taken";
 }
 
 else{
 
-	$check="SELECT * FROM companies WHERE name='$company' && location='$location'";
+	$check="SELECT id FROM companies WHERE name='$company_name' AND location='$location'";
+
 	$res=mysqli_query($conn, $check);
+
 	$counter=mysqli_num_rows($res);
 
 	if($counter == 1){
 
-		$getComp_id="SELECT id FROM companies WHERE name = '$company'"; 
+		while($row=mysqli_fetch_row($res)){
 
-		$reg="INSERT INTO users SET first_name='$first_name', last_name='$last_name', username='$username', email='$email', password='$password', companies_id='$getComp_id', user_types_id='1'";
+			$query="INSERT INTO `users`(`first_name`, `last_name`, `username`, `email`, `password`, `companies_id`, `user_types_id`) VALUES ('$first_name','$last_name','$username','$email','$password',$row[0], 1)";
 
-		mysqli_query($conn, $reg);
+			$insert=mysqli_query($conn,$query);
 
-		echo "Registration Successful";
+			if(!$insert){
 
-	}
-	else{
+				echo "There are some problems while inserting data";
+            }
+            else{
 
-		$comp ="INSERT INTO companies SET name='$company', location='$location'"; 
+            	echo "Data Inserted";
 
-		$res=mysqli_query($conn, $comp);
+            }
 
-		$getComp_id="SELECT id FROM companies WHERE name = '$company'"; 
+        }
+    }
 
-		$reg="INSERT INTO users SET first_name='$first_name', last_name='$last_name', username='$username', email='$email', password='$password', companies_id='$getComp_id', user_types_id=1";
+    else{
 
-		mysqli_query($conn, $reg);
+    	$comp="INSERT INTO `companies`(`name`, `location`) VALUES ('$company_name','$location')";
 
-		echo "Registration Successful";
-	}
+    	$res=mysqli_query($conn, $comp);
+
+    	$check="SELECT id FROM companies WHERE name='$company_name' AND location='$location'";
+
+    	$res=mysqli_query($conn, $check);
+
+    	$counter=mysqli_num_rows($res);
+
+    	if($counter == 1){
+
+    		while($row=mysqli_fetch_row($res)){
+
+    			$query="INSERT INTO `users`(`first_name`, `last_name`, `username`, `email`, `password`, `companies_id`, `user_types_id`) VALUES ('$first_name','$last_name','$username','$email','$password',$row[0], 1)";
+
+    			$insert=mysqli_query($conn,$query);
+
+    			if(!$insert){
+
+    				echo "There are some problems while inserting data";
+
+    			}
+                else{
+
+                	echo "Data Inserted";
+
+                }
+
+            }
+
+        }
+    }
+
 }
+
 ?>
