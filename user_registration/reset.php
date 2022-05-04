9ea659d1e1677527b73428ba9d229a171d72e16c
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2022-05-04 18:34:20 +0300

    Update reset.php

--- user_registration/reset.php
+++ user_registration/reset.php
@@ -6,7 +6,8 @@ $conn = mysqli_connect('localhost', 'root', '');
 mysqli_select_db($conn, 'companydb');
 
 $email=$_POST['email'];
-$password=$_POST['password'];
+$password=mysqli_real_escape_string($conn,$_POST['password']);
+$hash = hash('sha256', $password);
 
 $checker = "SELECT * FROM users WHERE email='$email'";
 $result=mysqli_query($conn, $checker);
@@ -14,7 +15,7 @@ $result=mysqli_query($conn, $checker);
 $num=mysqli_num_rows($result);
 if($num == 1){
 
-	$reg="UPDATE users SET password='$password' WHERE email='$email'";
+	$reg="UPDATE users SET password='$hash' WHERE email='$email'";
 	mysqli_query($conn, $reg);
 
 	header('location:page-login.php');
