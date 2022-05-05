ad65fb1a3f339e407969477aefab3b502aa62a5f
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2022-05-05 12:18:13 +0300

    Update validation.php

--- user_registration/validation.php
+++ user_registration/validation.php
@@ -6,9 +6,9 @@ mysqli_select_db($conn, 'companydb');
 
 $username=mysqli_real_escape_string($conn,$_POST['username']);
 $password=mysqli_real_escape_string($conn,$_POST['password']);
+$hash = hash('sha256', $password);
 
-
-$checker = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+$checker = "SELECT * FROM users WHERE username='$username' AND password='$hash'";
 $result=mysqli_query($conn, $checker);
 
 
@@ -16,11 +16,10 @@ $num=mysqli_num_rows($result);
 
 if($num == 1){
 	$_SESSION['username']=$username;
-	header('location:../startup/index.html');
+	header('location:../startup/index.php');
 }
 
 else{
 	header('location:page-login.php');
-	
 }
 ?>
